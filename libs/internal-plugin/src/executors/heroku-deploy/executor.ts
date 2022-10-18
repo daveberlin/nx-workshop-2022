c55f0c61e84a6a285b18acb0f3eb06207f2baacb
Dave Berlin <daveberlin@fico.com>
2022-10-18 16:56:40 -0700

    Simplify action Fix store deploy > build dep Add verbose to heroku push

--- libs/internal-plugin/src/executors/heroku-deploy/executor.ts
+++ libs/internal-plugin/src/executors/heroku-deploy/executor.ts
@@ -4,7 +4,7 @@ import { execSync } from 'child_process';
 export default async function runExecutor(schema: HerokuDeployExecutorSchema) {
   const cwd = schema.distLocation;
   execSync(`heroku container:login`, { cwd, stdio: 'inherit' });
-  execSync(`heroku container:push web --app ${schema.herokuAppName}`, { cwd, stdio: 'inherit' });
+  execSync(`heroku container:push web --app ${schema.herokuAppName} --verbose`, { cwd, stdio: 'inherit' });
   execSync(`heroku container:release web --app ${schema.herokuAppName}`, {
     cwd, stdio: 'inherit'
   });
